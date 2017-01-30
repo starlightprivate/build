5792ac5a603146d990feb9b546cb354d6ca3c13b
coderover <coderover@yahoo.com>
2017-01-30 23:26:46 +0500

    container build setup

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -rf app-server
+
+mkdir app-server
+
+cp -r ../node-api/.dist/* ./app-server
+
+rm -rf app-server/public
+mkdir ./app-server/public
+
+cp -r ../flashlightsforever/tacticalsales/*  ./app-server/public
+
