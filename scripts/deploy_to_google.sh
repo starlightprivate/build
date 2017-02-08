186fcf40ae675f30ff109d66626968ea772daa12
Evan Rusin <evan.rusin@outlook.com>
2017-02-08 20:30:06 +0200

    fix for deployment

--- scripts/deploy_to_google.sh
+++ scripts/deploy_to_google.sh
@@ -6,13 +6,17 @@ GOOGLE_CONTAINER_NAME=gcr.io/steady-computer-156807/flashlight-staging
 KUBERNETES_APP_NAME=flashlight-staging
 BUCKET_NAME=gs://flashlights-staging
 UPLOAD_FILE_NAME=$BUCKET_NAME/datefile
-DEFAULT_ZONE=us-central1-c
+DEFAULT_ZONE=us-east1-b
 
 codeship_google authenticate
 
 # Set the default zone to use
 echo "Setting default timezone $DEFAULT_ZONE"
 gcloud config set compute/zone $DEFAULT_ZONE
+
+echo "Connect to clusters"
+gcloud container clusters get-credentials flashlight-staging \
+    --zone us-east1-b --project steady-computer-156807
 
 echo "Deploying image on GCE"
 kubectl run $KUBERNETES_APP_NAME --image=$GOOGLE_CONTAINER_NAME --port=8080
